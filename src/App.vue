a61c169dce81625eb0956cb950c9815840a2d9d9
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-03-31 15:21:27 +0200

    Creazione di input e button

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { store } from './store.js'
 
 import MyHeader from './components/MyHeader.vue'
-import MyHeader from './components/MyHeader.vue'
+
 
 export default {
   components: {
@@ -15,7 +15,20 @@ export default {
 
     }
 
-  }, methods: {
+  },
+  methods: {
+    getFilms() {
+      let urlApiFilm = "https://api.themoviedb.org/3/search/movie?api_key=0e52fe5f3a026a87f645a2c5c7f70271&query=avengers";
+
+      axios.get(urlApiFilm)
+        .then(response => {
+          this.store.filmList = response.data.results;
+        })
+
+    },
+
+
+
 
   }
 }
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,11 +1,26 @@
 <script>
+import { store } from '../store.js';
 export default {
-    name: 'MyHeader'
+    name: 'MyHeader',
+    data() {
+        return {
+
+        }
+    }
 
 }
 </script>
 
-<template></template>
+<template>
+    <div class="search-bar">
+        <input type="text" placeholder="Cerca il tuo Film!">
+        <button>Cerca!</button>
+    </div>
+</template>
 
-
-<script scoped></script>
+<style scoped>
+.search-bar {
+    text-align: center;
+    margin-top: 30px;
+}
+</style>
